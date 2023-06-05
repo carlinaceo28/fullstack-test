e8775f58c3dc4f256c6585ab83418edef3d1e193
raulhenrique-x3 <raulheri43@hotmail.com>
2023-06-04 21:41:56 -0300

    mongoose

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import app from "./index";
 
 const connectDB = async () => {
-  mongoose
+  mongoose.set("strictQuery", false);
+
+  await mongoose
     .connect(process.env.DB_URI!)
     .then(() => console.log("Connected to DB"))
     .catch((err: any) => console.error("Could not connect", err));
