2aad8ebc0848496a194161be9d3549e6c985d6fe
Raul Henrique <raulheri43@hotmail.com>
2023-06-05 16:55:59 -0300

    Update verificacaoDeIdade.ts

--- server/src/middlewares/verificacaoDeIdade.ts
+++ server/src/middlewares/verificacaoDeIdade.ts
@@ -1,24 +1,30 @@
-import { NextFunction, Request, Response } from "express";
-
 function isOver18(req: Request, res: Response, next: NextFunction) {
-  const { dataDeNascimento } = req.body
+  const { dataDeNascimento } = req.body;
   if (!dataDeNascimento) {
-    res.status(400).send({ message: 'Data de nascimento inválida!' });
-    return;
+    return res.status(400).send({ message: 'Data de nascimento inválida!' });
   }
 
   const [day, month, year] = dataDeNascimento.split('/').map(Number);
+
+  if (month > 12) {
+    return res.status(400).send({ message: 'Mês inválido!' });
+  }
+
   const birthDate = new Date(year, month - 1, day);
   const currentDate = new Date();
   const ageDiffMs = currentDate.getTime() - birthDate.getTime();
   const ageDate = new Date(ageDiffMs);
   const age = Math.abs(ageDate.getUTCFullYear() - 1970);
-  if (age >= 18) {
-    next();
-  }  else {
+
+  if (age < 18) {
     return res.status(403).send({ message: 'O aluno precisa ser maior de 18 anos!' });
   }
-  return
+
+  if (age > 99) {
+    return res.status(403).send({ message: 'O aluno não pode ter mais de 99 anos!' });
+  }
+
+  next();
 }
 
 export default isOver18;
