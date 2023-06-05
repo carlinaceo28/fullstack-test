4d63a840ec87038c36623a5b0881de7d72e60c05
Raul Henrique <raulheri43@hotmail.com>
2023-06-05 17:41:57 -0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,6 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
