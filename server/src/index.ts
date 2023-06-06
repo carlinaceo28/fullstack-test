2da364326d5023989b9103ce590315f94f97943f
Raul Henrique <raulheri43@hotmail.com>
2023-06-06 08:52:56 -0300

    Update cors no index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,13 @@ import cors from "cors";
 import router from "./routes/Router";
 import dotenv from "dotenv";
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
