2251f83a71c5140f95ab7024c84edee58c4d60ff
Raul Henrique <raulheri43@hotmail.com>
2023-06-06 10:15:47 -0300

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,21 +3,25 @@ import mongoose from "mongoose";
 import app from "./index";
 
 const connectDB = async () => {
-  mongoose.set("strictQuery", false);
+  try {
+    mongoose.set("strictQuery", false);
 
   await mongoose
     .connect(process.env.DB_URI!)
     .then(() => console.log("Connected to DB"))
     .catch((err: any) => console.error("Could not connect", err));
+  } catch (error) {
+    console.error("Erro no db.ts", error);
+  }
 
-  app.use((err: Error, req: Request, res: Response) => {
-    if (err instanceof Error) {
-       return res.status(500).send({
-      status: "error",
-      message: `Internal server error - ${err}`,
-    });
-    }
-  });
+//   app.use((err: Error, req: Request, res: Response) => {
+//     if (err instanceof Error) {
+//        return res.status(500).send({
+//       status: "error",
+//       message: `Internal server error - ${err}`,
+//     });
+//     }
+//   });
 };
 
 export default connectDB;
